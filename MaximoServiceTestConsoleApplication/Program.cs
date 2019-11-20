788b40f2dfc195e58c280f61197fe75d9a118a55
Emre Delioğlu <deliiemre@gmail.com>
2019-11-21 01:29:12 +0300

    Appended FindByAssetAssetNum function at WorkOrder and tested it

--- MaximoServiceTestConsoleApplication/Program.cs
+++ MaximoServiceTestConsoleApplication/Program.cs
@@ -9,27 +9,36 @@ namespace MaximoServiceTestConsoleApplication
     {
         public static void Main(string[] args)
         {
-            MaximoService maximoService = new MaximoService();
-
-            bool isLoggedIn = maximoService.login("erdem", "password");
-            Console.WriteLine($"is user Logged in Maximo: {isLoggedIn}");
-            
-            maximoService.whoami();
-            MaximoUser maximoUser = maximoService.mxuser;
-            
-            Console.WriteLine($"userid: {maximoUser.userName}");
-          
+//            MaximoService maximoService = new MaximoService();
+//
+//            bool isLoggedIn = maximoService.login("erdem", "password");
+//            Console.WriteLine($"is user Logged in Maximo: {isLoggedIn}");
+//            
+//            maximoService.whoami();
+//            MaximoUser maximoUser = maximoService.mxuser;
+//            
+//            Console.WriteLine($"userid: {maximoUser.userName}");
+//          
             
             MaximoWorkOrder wo = new MaximoWorkOrder();
             wo.wonum = "123123";
             wo.locationdetails = "First AVE";
-
+            MaximoAsset asset = new MaximoAsset();
+            asset.assetnum = "234234";
+            asset.assetid = 3.14;
+            wo.asset = asset;
             WorkOrder.Insert(wo);
 
             MaximoWorkOrder wo2 = WorkOrder.FindByWoNum("123123");
             Console.WriteLine(wo2.locationdetails);
+            
+            MaximoWorkOrder wo3 = WorkOrder.FindByAssetAssetNum("234234");
+            Console.WriteLine(wo3.asset.assetid);
             //list work orders
-
+            
         }
     }
 }
+
+
+
