853ef95ba8e48f1d9c7d6d0f04e9a172daabc660
Emre Delioğlu <deliiemre@gmail.com>
2019-11-21 00:42:20 +0300

    Created class to test with MaximoWorkOrder using by liteDB

---
+++ LocalDBLibrary/WorkOrder.cs
@@ -0,0 +1,30 @@
+using LiteDB;
+using MaximoServiceLibrary.model;
+
+namespace LocalDBLibrary
+{
+    public class WorkOrder
+    {
+        
+        // todo : move app config
+        private static string dbString = "lite.db";
+        
+        public static void Insert(MaximoWorkOrder workOrder)
+        {
+            using (var db = new LiteDatabase(@dbString))
+            {
+                var workorders = db.GetCollection<MaximoWorkOrder>("workorders");
+                workorders.Insert(workOrder);
+            }
+        }
+
+        public static MaximoWorkOrder FindByWoNum(string num)
+        {
+            using (var db = new LiteDatabase(@dbString))
+            {
+                var workorders = db.GetCollection<MaximoWorkOrder>("workorders");
+                return workorders.FindOne(x => x.wonum.Contains(num));
+            }
+        }
+    }
+}
