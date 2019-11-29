8acbcd2412b6ac9c34241d2957a5c213fd09076a
emredelioglu <emredelioglu@CORP.AMAZONWORKSPACES.COM>
2019-11-29 13:29:43 +0300

    create labor and tool window

---
+++ CatchBasin/ViewModel/Command/LaborCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CatchBasin.ViewModel.Command
+{
+	class LaborCommand : ICommand
+	{
+		public event EventHandler CanExecuteChanged
+		{
+			add
+			{
+				CommandManager.RequerySuggested += value;
+			}
+			remove
+			{
+				CommandManager.RequerySuggested -= value;
+			}
+		}
+		
+		public LaborCommand()
+		{
+			
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			
+		}
+	}
+}
